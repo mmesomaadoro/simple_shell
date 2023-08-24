b8e8cd64c770e450a49ddf493ce2a6e44f386966
mmesomaadoro <adorojanefrances@gmail.com>
2023-08-24 16:05:52 -0700

    env

--- env.c
+++ env.c
@@ -2,20 +2,20 @@
 
 /**
  * _envHandler - function that prints env variables
- *  @env: environment variables
+ *  @envp: environment variables
  *  @av: argument vector
  *  Return: void
  */
-void _envHandler(char **env, char **av)
+void _envHandler(char **envp, char **av)
 {
 	size_t i, len;
 
 	if (_strcmp(av[0], "env") == 0)
 	{
-		for (i = 0; env[i] != NULL; i++)
+		for (i = 0; envp[i] != NULL; i++)
 		{
-			len = _strlen(env[i]);
-			write(STDOUT_FILENO, env[i], len);
+			len = _strlen(envp[i]);
+			write(STDOUT_FILENO, envp[i], len);
 			write(STDOUT_FILENO, "\n", 1);
 		}
 	}
